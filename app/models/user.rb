edd8eb19cc1cbdadff4725a8f7b6acc8436f005a
Ryan Gum <ryangum1@gmail.com>
2014-09-15 23:09:07 +0200

    Updated front end for the new authentication system
    Added simple_form
    Updated several forms to boostrap and simple_form
    Added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,31 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+        
 
-  
+  has_many :statuses
+
+  # def full_name
+  # 	if first_name and last_name
+  #   	first_name + " " + last_name
+  #   else
+  #   	"Has no name"
+  #   end
+  # end
+
+
+  def full_name
+   if self.first_name.blank? && self.last_name.blank?
+        return "has no name"
+   else
+        if self.first_name.blank?
+            return self.last_name
+        elsif self.last_name.blank?
+            return self.first_name
+        else
+             return self.first_name + " " + self.last_name
+        end
+   end
+  end
 
 end
