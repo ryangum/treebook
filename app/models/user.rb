a31301ea480d68244ff9a6f87f0db08301d36ec0
Ryan Gum <ryangum1@gmail.com>
2014-09-18 21:55:42 +0200

    added custom routes for auth and feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,27 @@ class User < ActiveRecord::Base
 
   has_many :statuses
 
-  # def full_name
-  # 	if first_name and last_name
-  #   	first_name + " " + last_name
-  #   else
-  #   	"Has no name"
-  #   end
-  # end
-
-
   def full_name
-   if self.first_name.blank? && self.last_name.blank?
-        return "has no name"
-   else
-        if self.first_name.blank?
-            return self.last_name
-        elsif self.last_name.blank?
-            return self.first_name
-        else
-             return self.first_name + " " + self.last_name
-        end
-   end
+  	# if first_name and last_name
+    	first_name + " " + last_name
+    # else
+    # 	"Has no name"
+    # end
   end
+
+
+  # def full_name
+  #  if self.first_name.blank? && self.last_name.blank?
+  #       return "has no name"
+  #  else
+  #       if self.first_name.blank?
+  #           return self.last_name
+  #       elsif self.last_name.blank?
+  #           return self.first_name
+  #       else
+  #            return self.first_name + " " + self.last_name
+  #       end
+  #  end
+  # end
 
 end
